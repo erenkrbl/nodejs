46d38e37097c20ff9ca712708c5ec18c68c8d6ba
erenkrbl <ekurubal24@gmail.com>
2023-09-25 21:45:31 +0300

    HTTP_Methods ,  route

--- class-notes/08-ExpressJS/app.js
+++ class-notes/08-ExpressJS/app.js
@@ -1,5 +1,46 @@
-"use strict";
+"use strict"
+/* -------------------------------------------------------
+    EXPRESSJS
+------------------------------------------------------- */
+/*
+ * npm init -y
+ * npm i express dotenv
+*/
 
-const express = require("express");
+/* ExpressJS Start */
+const express = require('express'); // Assing expressFramework to express variable.
+const app = express() // run application on express.
 
-const app = express();
+/* ENV */
+require('dotenv').config()
+const HOST = process.env.HOST || '127.0.0.1'
+const PORT = process.env.PORT || 8000
+/* ------------------------------------------------------- */
+/* HTTP_Methods & URLs */
+
+app.get('/', (request, response) => {
+    //? run response.send for print-out:
+    // response.send( 'Welcome to Express' )
+    response.send({ message: "called in 'get' method." })
+})
+app.post('/', (request, response) => response.send({ message: "called in 'post' method."}))
+app.put('/', (request, response) => response.send({ message: "called in 'put' method."}))
+app.delete('/', (request, response) => response.send({ message: "called in 'delete' method."}))
+// //? allow at all methods:
+// app.all('/', (request, response) => response.send({ message: "'all' option allows to all methods."}))
+
+//? app.route('url'):
+app.route('/route')
+    .get( (req, res) => res.send('get') )
+    .post( (req, res) => res.send('post') )
+    .put( (req, res) => res.send('put') )
+    .delete( (req, res) => res.send('delete') )
+
+/* ------------------------------------------------------- */
+/* URL Options */
+
+
+
+/* ------------------------------------------------------- */
+// app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`))
+app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`))
