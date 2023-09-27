49b5246bc27931bf42c1ed0f0bcffac012f691b9
erenkrbl <ekurubal24@gmail.com>
2023-09-27 22:35:12 +0300

    added express, middleware

--- class-notes/08-ExpressJS/app.js
+++ class-notes/08-ExpressJS/app.js
@@ -47,28 +47,68 @@ const PORT = process.env.PORT || 8000
 // app.get('/abc*123', (req, res) => res.send("in 'abc*123'")) // abc123 or abc...123 // abc(ANY)123
 //? express-urls supported regexp:
 // app.get(/xyz/, (req, res) => res.send("regexp /xyz/")) // url contains = 'xyz' (no limit for subPaths)
-app.get(/^\/xyz/, (req, res) => res.send("regexp /^xyz/")) // url startswith = 'xyz'
+// app.get(/^\/xyz/, (req, res) => res.send("regexp /^\/xyz/")) // url startswith = 'xyz'
 // app.get(/xyz$/, (req, res) => res.send("regexp /xyz$/")) // url endswith = 'xyz'
 
 /* ------------------------------------------------------- */
 /* URL Parameters (req.params) */
 
 // /user/66/config/update/any/any/any
-app.get('/user/:userId/config/:configParam/*', (req, res) => {
-    res.send({
-        url: {
-            protocol: req.protocol,
-            subdomains: req.subdomains,
-            hostname: req.hostname,
-            baseUrl: req.baseUrl,
-            params: req.params,
-            query: req.query,
-            path: req.path,
-            originalUrls: req.originalUrl
-        }
-    })
-})
+// app.get('/user/:userId/config/:configParam/*', (req, res) => {
+//     res.send({
+//         userId: req.params.userId,
+//         configParam: req.params.configParam,
+//         url: {
+//             protocol: req.protocol,
+//             subdomains: req.subdomains,
+//             hostname: req.hostname,
+//             baseUrl: req.baseUrl,
+//             params: req.params,
+//             query: req.query,
+//             path: req.path,
+//             originalUrls: req.originalUrl
+//         }
+//     })
+// })
+
+// //? '\d' means only-digit-chars in regexp:
+// //? '\w' means only-chars in regexp:
+// // app.get('/user/:userId([0-9]+)', (req, res) => {
+// app.get('/user/:userId(\\d+)', (req, res) => {
+//     res.send({
+//         params: req.params
+//     })
+// })
+
+// app.get('/command/:userId-:profileId', (req, res) => {
+//     res.send({
+//         params: req.params
+//     })
+// })
+
+/* ------------------------------------------------------- */
+
+/* Response Methods */
+
+//? SendStatus:
+// app.get('/', (req, res) => res.sendStatus(404))
+//? Status:
+// app.get('/', (req, res) => res.status(200).send({ message: 'OK' }))
+// app.post('/', (req, res) => res.status(201).send({ message: 'Created' }))
+// app.put('/', (req, res) => res.status(202).send({ message: 'Accepted' }))
+// app.delete('/', (req, res) => res.status(204).send({ message: 'No Content' }))
+//? JSON (.send() method already does this converting.)
+// app.get('/', (req, res) => res.json([{ key: 'value' }]))
+//? Download File (Download at browser):
+// app.get('/download', (req, res) => res.download('app.js', 'changedName.js'))
+//? SendFile Content:
+// console.log( __dirname )
+// app.get('/file', (req, res) => res.sendFile(__dirname + '/app.js')) // FilePath must be realPath
+//? Redirect:
+// app.get('/google', (req, res) => res.redirect(302, 'https://www.google.com'))
+
 
 /* ------------------------------------------------------- */
 // app.listen(PORT, () => console.log(`Running on http://127.0.0.1:${PORT}`))
 app.listen(PORT, HOST, () => console.log(`Running on http://${HOST}:${PORT}`))
+
